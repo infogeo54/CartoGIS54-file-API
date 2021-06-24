59c255b2fbd7424770d7ec926aa6ac3200a9a24c
infogeo54 <infogeo54@departement54.fr>
2021-06-24 16:35:03 +0200

    Add Allowed Domains in config file + commands

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import formData from 'express-form-data';
 import os from 'os';
 import router from './routes/index.mjs';
 import apiErrorHandler from './error/api-error-handler.mjs';
+import { readFile } from "fs/promises" 
 
 const app = express();
 const formDataOptions = {
@@ -18,10 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }))
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
-    res.header("Acces-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+app.use(async function (req, res, next) {
+    const domainsAllowed = JSON.parse(await readFile(new URL('api.config.json', import.meta.url))).domainsAllowed
+    if (domainsAllowed.indexOf(req.headers.origin) !== -1){
+        res.header("Access-Control-Allow-Origin", req.headers.origin)
+        res.header("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+        res.header("Acces-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    }
     next();
 })
 
